cf4cebe9ea60c8d5523281a01f587157becdfb92
emonShimoul <emonshimoul@gmail.com>
2022-10-03 00:08:57 +0600

    make mongodb connection

--- index.js
+++ index.js
@@ -11,7 +11,18 @@ app.use(cors());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pabg0.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri);
+// console.log(uri);
+
+async function run(){
+    try{
+        await client.connect();
+        console.log('database connected successfully');
+    }
+    finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello Doctors Portal!')
