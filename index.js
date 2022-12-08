a2ba1529b581564173233b509bb602301ed959c3
emonShimoul <emonshimoul@gmail.com>
2022-12-09 01:25:37 +0600

    added post API for doctors data

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
+const fileUpload = require('express-fileupload');
 
 const port = process.env.PORT || 5000;
 
@@ -29,6 +30,7 @@ admin.initializeApp({
 
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pabg0.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -51,6 +53,7 @@ async function run() {
     const database = client.db('doctors_portal');
     const appointmentsCollection = database.collection('appointments');
     const usersCollection = database.collection('users');
+    const doctorsCollection = database.collection('doctors');
 
     // GET API for appointments
     app.get('/appointments', async (req, res) => {
@@ -89,6 +92,12 @@ async function run() {
       };
       const result = await appointmentsCollection.updateOne(filter, updateDoc);
       res.json(result);
+    })
+
+    app.post('/doctors', async (req, res) => {
+      console.log('body', req.body);
+      console.log('files', req.files);
+      res.json({ success: true })
     })
 
     // GET API for specific user
